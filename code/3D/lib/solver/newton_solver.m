dc9c5a7690fefd61c782b75e2a8206fd04be79f6
Lucas Wilkinson <wilkinson.lucas@gmail.com>
2021-11-09 15:09:56 -0800

    Matlab PCG experimentation

--- code/3D/lib/solver/newton_solver.m
+++ code/3D/lib/solver/newton_solver.m
@@ -11,21 +11,43 @@ order = get_ordering(u);
 
 'newton'
 
-for i = 0 : 2000
- 
-    
-    %plot_result( u, i )
+x0 = zeros(length(u), 1);
 
+hold on
+for i = 0 : 2000
+    
     [grad, H] = grad_hessian_function(u, 0);
 
-    x0 = zeros(length(grad), 1);
-    [p, iterations, rnorm] = diag_conjgrad(H, grad, x0);
-    p(order) = -1.0 * H(order, order) \ grad(order);
+% Incomplete LU factorization, preconditioner
+    [L,U] = ilu(H, struct('type','nofill'));
 
+% Diagonal Preconditioner
+    % M = spdiags(1./diag(H), 0, length(H),length(H));
+     
+% Preconditioned CG
+
+    tol = 1e-4;
+    restart = 100;
+    maxit = 100;
+    iterations = 0;
+    rnorm = 0;
+    
+    [p, d, rnorm, iterations, resvec] = pcg(H, -1.0 * grad, tol, maxit, L,U);
+     
+% Plot PCG residual progress 
+    plot(resvec / norm(-1.0 * grad), 'DisplayName', ['iter ', num2str(i)]);
+    set(gca, 'YScale', 'log')
+     
+% Original Implementation
+%      p(order) = -1.0 * H(order, order) \ grad(order);
+    
     un = line_check_search(p, u, grad);
     
+% Warm start
+    x0 = p;
+    
     disp(['= Newton ', num2str(i), ' => ', ...
-        'num iter: ', num2str(iterations), ' res: ', num2str(rnorm)])
+        'num iter: ', num2str(iterations), ' res: ', num2str(rnorm), ' un: ', num2str(energy_value(un))])
     
     if stop_check(un, u, grad)
         
@@ -37,5 +59,6 @@ for i = 0 : 2000
 
 end
 
+legend();
 end
 
